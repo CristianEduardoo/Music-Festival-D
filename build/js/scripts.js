ec7e0ed9c526cf6bd0e598457c7860545b3d9d1d
CristianEduardoo <cj.94@hotmail.com>
2024-02-13 11:58:42 +0100

    agregandoFalsaPromesa

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,104 +1,112 @@
-document.addEventListener('DOMContentLoaded', function () {
-    inciarApp();
-})
+document.addEventListener("DOMContentLoaded", function () {
+  inciarApp();
+});
 
 function inciarApp() {
-    //reproduceVideo();
-    navegacionFija();//barra header fija
-    crearGaleria();
-    scrollNav(); //smoothScroll
+  reproduceVideo();
+  navegacionFija(); //barra header fija
+  crearGaleria();
+  scrollNav(); //smoothScroll
 }
 
 function navegacionFija() {
-    const barra = document.querySelector('.header');
-    const sobreFestival = document.querySelector('.sobre-festival');
-    const body = document.querySelector('body');
+  const barra = document.querySelector(".header");
+  const sobreFestival = document.querySelector(".sobre-festival");
+  const body = document.querySelector("body");
 
-    window.addEventListener('scroll', function(){
-        if(sobreFestival.getBoundingClientRect().bottom < 0 ){
-            barra.classList.add('fijo');
-            body.classList.add('body-scroll')
-        }else{
-            barra.classList.remove('fijo');
-            body.classList.remove('body-scroll');
-        }
-    })
+  window.addEventListener("scroll", function () {
+    if (sobreFestival.getBoundingClientRect().bottom < 0) {
+      barra.classList.add("fijo");
+      body.classList.add("body-scroll");
+    } else {
+      barra.classList.remove("fijo");
+      body.classList.remove("body-scroll");
+    }
+  });
 }
 
-function scrollNav(){
-    const enlaces = document.querySelectorAll('.navegacion-principal a');
-    enlaces.forEach(enlaces => {
-        enlaces.addEventListener('click', function(e){
-            e.preventDefault();
+function scrollNav() {
+  const enlaces = document.querySelectorAll(".navegacion-principal a");
+  enlaces.forEach((enlaces) => {
+    enlaces.addEventListener("click", function (e) {
+      e.preventDefault();
 
-            const seccionScroll = e.target.attributes.href.value;
-            const seccion = document.querySelector(seccionScroll);
-            seccion.scrollIntoView({behavior: "smooth"});
-        });
+      const seccionScroll = e.target.attributes.href.value;
+      const seccion = document.querySelector(seccionScroll);
+      seccion.scrollIntoView({ behavior: "smooth" });
     });
+  });
 }
 
 function crearGaleria() {
-    const galeria = document.querySelector('.galeria-imagenes');
+  const galeria = document.querySelector(".galeria-imagenes");
 
-    for (let i = 1; i <= 12; i++) {
-        const imagen = document.createElement('picture');
-        imagen.innerHTML = `
+  for (let i = 1; i <= 12; i++) {
+    const imagen = document.createElement("picture");
+    imagen.innerHTML = `
         <source srcset="build/img/pequeña/${i}.avif" type="image/avif">
         <source srcset="build/img/pequeña/${i}.webp" type="image/webp">
         <img loading="lazy" width="200" height="300" src="build/img/pequeña/${i}.jpg"
             alt="Imagen galeria"> <!-- fallback -->
         `;
 
-        imagen.onclick = function(){
-            mostrarImagen(i);
-        }
+    imagen.onclick = function () {
+      mostrarImagen(i);
+    };
 
-        galeria.appendChild(imagen);
-    }
+    galeria.appendChild(imagen);
+  }
 }
 
-function mostrarImagen(index){
-    const imagen = document.createElement('picture');
-    imagen.innerHTML = `
+function mostrarImagen(index) {
+  const imagen = document.createElement("picture");
+  imagen.innerHTML = `
     <source srcset="build/img/grande/${index}.avif" type="image/avif">
     <source srcset="build/img/grande/${index}.webp" type="image/webp">
     <img loading="lazy" width="200" height="300" src="build/img/grande/${index}.jpg"
         alt="Imagen galeria"> <!-- fallback -->
     `;
-    //Creando Overlay con la imagen
-    const overlay = document.createElement('DIV');//oscurecemos un poco la pantalla de fondo.
-    overlay.appendChild(imagen);
-    overlay.classList.add('overlay');
-    overlay.onclick = function (){
-        const body = document.querySelector('body');
-        body.classList.remove('fijar-body');
-        overlay.remove();
-    }
+  //Creando Overlay con la imagen
+  const overlay = document.createElement("DIV"); //oscurecemos un poco la pantalla de fondo.
+  overlay.appendChild(imagen);
+  overlay.classList.add("overlay");
+  overlay.onclick = function () {
+    const body = document.querySelector("body");
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
 
-    //Boton para cerrar el modal
-    const cerrarModal = document.createElement('P');
-    cerrarModal.textContent = 'X';
-    cerrarModal.classList.add('btn-cerrar');
-    cerrarModal.onclick = function(){
-        const body = document.querySelector('body');
-        body.classList.remove('fijar-body');
-        overlay.remove();
-    }
-    overlay.appendChild(cerrarModal);
+  //Boton para cerrar el modal
+  const cerrarModal = document.createElement("P");
+  cerrarModal.textContent = "X";
+  cerrarModal.classList.add("btn-cerrar");
+  cerrarModal.onclick = function () {
+    const body = document.querySelector("body");
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
+  overlay.appendChild(cerrarModal);
 
-    //Añadiendo al HTML
-    const body = document.querySelector('body');
-    body.appendChild(overlay);//asi mostramos en el body la imagen
-    body.classList.add('fijar-body');
+  //Añadiendo al HTML
+  const body = document.querySelector("body");
+  body.appendChild(overlay); //asi mostramos en el body la imagen
+  body.classList.add("fijar-body");
 }
 
 function reproduceVideo() {
   const video = document.querySelector("#videoJS");
+  let playing = 0;
 
-  video.play();
-  // Configurar propiedades del video
-  video.autoplay = true;
-  video.muted = true;
-  video.loop = true;
+  let promise = video.play();
+  if (promise !== undefined) {
+    promise
+      .then((_) => {
+        // Autoplay started!
+      })
+      .catch((error) => {
+        // Autoplay was prevented.
+        // Show a "Play" button so that user can start playback.
+        // alert ("No hay autoplay, debemos provocarlo");
+      });
+  }
 }
